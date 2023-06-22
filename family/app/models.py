516bbde07f7d271053c47bc6a5d42e29d9bd3921
Matias Gigena <mati-mangekyou@hotmail.com>
2023-06-22 10:47:28 -0300

    Eliminacion de Enique=True

--- family/app/models.py
+++ family/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Familiar(models.Model):
-    name=models.CharField(max_length=25, unique=True)
+    name=models.CharField(max_length=25)
     edad=models.IntegerField()
     parentesco=models.CharField(max_length=15)
     fecha_ingreso=models.DateField(auto_now_add=True,null=True, blank=True)
